4612a665e3a4e71b63ca0310ac2a0165f230938e
DEEPAK1681 <gargdeepak897@gmail.com>
2026-01-30 20:29:42 +0530

    Add files via upload

---
+++ courses/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import User, Course, Module, Enrollment
+from django.contrib.auth.admin import UserAdmin
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Course)
+admin.site.register(Module)
+admin.site.register(Enrollment)
